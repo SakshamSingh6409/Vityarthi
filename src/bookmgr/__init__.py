128b25e519e90ff76d95345138f5d69cd7eac99a
SakshamSingh2319 <98216435+SakshamSingh6409@users.noreply.github.com>
2025-11-25 16:16:05 +0530

    Delete src directory

--- src/bookmgr/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-bookmgr: simple JSON-backed book manager CLI.
-"""
-__all__ = []
-__version__ = "0.1.0"
-
--- src/bookmgr/main.py
+++
@@ -1,7 +0,0 @@
-from cli import run
-
-def main() -> None:
-    run()
-
-if __name__ == "__main__":
-    main()
